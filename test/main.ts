65dc9ef3461164bc29c82d0d0329a98e1ae730eb
Wojtek Przechodzeń <wprzecho@gmail.com>
2017-12-24 09:53:48 +0100

    initial commit

--- test/main.ts
+++
@@ -1,5 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './component/app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule);
